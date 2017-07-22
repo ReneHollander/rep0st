4a80155099e17aeacfa6a6cfc98a1da854a191f4
Rene Hollander <mail@renehollander.at>
2017-07-22 20:23:51 +0100

    flag api for posts

--- test.py
+++ test.py
@@ -6,7 +6,16 @@ rep = config.create_rep0st()
 
 image = cv2.imread(str('test.jpg'), cv2.IMREAD_COLOR)
 
+print(rep.get_statistics())
+
+post = rep.database.get_post_by_id(2046642)
+print(post)
+print(post.get_flags())
+
+exit(0)
+
 res = rep.get_index().search(image)
 print(res)
+print(rep.get_statistics())
 
 # rep.close()
